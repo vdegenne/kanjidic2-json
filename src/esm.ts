b54d15e3a3a5314fa0158ebb46068b88345414ed
Valentin Degenne <vdegenne@gmail.com>
2025-04-10 21:10:12 +0200

    0.0.1

--- src/esm.ts
+++ src/esm.ts
@@ -2,4 +2,6 @@
 import _data from '../KANJIS.json' with {type: 'json'};
 import type {KanjiDic2Character} from './types.js';
 
+export {type KanjiDic2Character};
+
 export default _data as KanjiDic2Character[];
